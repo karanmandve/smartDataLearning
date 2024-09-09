6ac70dccb45aa531bc1eac801383c4955f66f744
Karan Mandve <karanmandve@sdeichd.com>
2024-09-09 16:37:33 +0530

    initial commit

---
+++ CSharp_basics/CSharp_basics/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_basics
+{
+    public abstract class Animal
+    {
+        // Abstract method: No implementation in the base class
+        public abstract void Speak();
+
+
+        // Regular method with implementation
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        // Implementing the abstract method
+        public override void Speak()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
+}
---
+++ CSharp_basics/CSharp_basics/Bike.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_basics
+{
+    public interface IDriveable
+    {
+        void Start();
+        void Stop();
+    }
+    public interface IOffice
+    {
+        void PunchIn();
+        void PunchOut();
+    }
+
+    public class SmartData: IOffice
+    {
+        public void PunchIn()
+        {
+            Console.WriteLine("Bike started.");
+        }
+
+        public void PunchOut()
+        {
+            Console.WriteLine("Bike stopped.");
+        }
+    }
+
+    public class Bike : IDriveable
+    {
+        public void Start()
+        {
+            Console.WriteLine("Bike started.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Bike stopped.");
+        }
+    }
+    public class Cycle : IDriveable
+    {
+        public void Start()
+        {
+            Console.WriteLine("Cycle started.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Bike stopped.");
+        }
+    }
+}
