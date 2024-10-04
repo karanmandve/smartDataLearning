cdd96c95b8572c2bf392e60c69533d75b2497a0f
Karan Mandve <karanmandve@sdeichd.com>
2024-10-04 10:23:29 +0530

    file deleted

--- .NET/LogValidationExceptionExample/LogValidationExceptionExample/Models/TestModelDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LogValidationExceptionExample.Models
-{
-    public class TestModelDto
-    {
-        public int StudentId { get; set; }
-        public string Name { get; set; }
-    }
-}
